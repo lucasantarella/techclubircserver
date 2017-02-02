f5b139e48c691dd7760fdda84e034a33dd38aa14
Luca Santarella <luca.santarella@gmail.com>
2017-02-02 15:05:13 -0500

    20170119

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET api listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/people', function (req, res, next) {
+    res.send({
+        people: [
+            {
+                name: "Charlie",
+                special: true,
+                extra_special: "true-er"
+            },
+            {
+                name: "Jack",
+                special: true,
+                extra_special: true
+            }
+        ]
+    });
+});
+
+router.get('/test', function (req, res, next) {
+    connection.query('SHOW TABLES;',{}, function (response) {
+        res.send(response);
+    });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/charlie', function (req, res, next) {
+    var date = new Date();
+
+    res.render('charlie', {
+        time: date.getTime()
+    })
+});
+
+
+router.get('/people', function (req, res, next) {
+    res.render('people');
 });
 
 module.exports = router;
